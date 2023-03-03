20161daa817574f2aeda6f7c37811391e2c7d7bc
kenjopa <kenjopa6@gmail.com>
2023-03-03 09:30:28 +0500

    added pagination

---
+++ src/components/PaginationButtons.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+<template>
+    <div class="pagination">
+        <button class="btn pagination__btn" @click="$emit('reducePage')">
+            &lt;
+        </button>
+        <button class="btn pagination__btn" @click="$emit('increasePage')">
+            &gt;
+        </button>
+    </div>
+</template>
