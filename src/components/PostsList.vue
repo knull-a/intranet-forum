ac5fc652c33a54a758d3c8daef909cf3aec88099
k-nulla <kenjopa6@gmail.com>
2023-03-10 19:25:06 +0500

    removed api

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,15 +1,11 @@
 <script setup>
-import { ref, watch } from "vue";
-import axios from "axios";
+import { ref, watch, computed } from "vue";
 import PaginationButtons from "./PaginationButtons.vue";
-import SearchBox from "./SearchBox.vue";
 
-const input = ref("");
+// const input = ref("");
 
-const posts = ref(null);
+// const posts = ref(null);
 const page = ref(1);
-
-const img = ref("https://picsum.photos/id/");
 
 const increasePage = () => {
   if (page.value >= 10) return;
@@ -21,64 +17,140 @@ const reducePage = () => {
   page.value--;
 };
 
-const response = await axios.get(
-  `https://jsonplaceholder.typicode.com/posts?_page=1}`
-);
-posts.value = response.data;
+// const response = await axios.get(
+//   `https://jsonplaceholder.typicode.com/posts`
+// );
 
-watch(page, async () => {
-  const res = await axios.get(
-    `https://jsonplaceholder.typicode.com/posts?_page=${page.value}`
-  );
-  posts.value = res.data;
-});
+// posts.value = response.data;
 
-watch(
-  input,
-  () =>
-    (posts.value = posts.value.filter((item) =>
-      item.title.toLowerCase().includes(input.value.toLowerCase())
-    ))
-), {immediate: true};
+const selectOptions = (e) => {
+  attr.value = e.target.value;
+  isAscending.value = !isAscending.value;
+};
+
+// watch(
+//   input,
+//   () =>
+//     (posts.value = posts.value.filter((item) =>
+//       item.title.toLowerCase().includes(input.value.toLowerCase())
+//     ))
+// ), {immediate: true};
 
 const isAscending = ref(true);
 
 const attr = ref("id");
-
-const selectOptions = (e) => {
-  attr.value = e.target.value
-  isAscending.value = !isAscending.value
-};
 
 watch(isAscending, () => {
   isAscending.value === true
     ? posts.value.sort((a, b) => (a[attr.value] < b[attr.value] ? -1 : 1))
     : posts.value.sort((a, b) => (a[attr.value] > b[attr.value] ? -1 : 1));
 });
+
+const posts = ref([]);
+
+const isPopupOpened = ref(false);
+
+function addPost() {
+  posts.value.push({
+    title: title.value,
+    body: body.value,
+    id: Number(new Date()),
+  });
+  title.value = "";
+  body.value = "";
+  isPopupOpened.value = false;
+}
+
+const removePost = (id) =>
+  (posts.value = posts.value.filter((post) => post.id != id));
+
+const title = ref("");
+const body = ref("");
+const id = ref(1);
+
+// posts.value.push(array[2])
+// console.log(posts.value);
 </script>
 
 <template>
-  <SearchBox v-model:input.trim="input"/>
-  <div name="sort" @click="selectOptions">
-    <button value="id">Id</button>
-    <button value="title">Title</button>
-    <button value="body">Body</button>
-  </div>
-  <div class="post" v-for="post in posts" :key="post.id">
-    <div class="post__info">
-      <div class="post__info_image">
-        <img :src="img + post.id + '/' + 200" />
-      </div>
-      <div class="post__info_text">
-        <span class="post__user-id">User Id: {{ post.userId }} </span>
-        <span class="post__id"> Id: {{ post.id }}</span>
-        <h2 class="post__title">{{ post.title }}</h2>
-        <p class="post__body">
-          {{ post.body }}
-        </p>
+  <div class="posts__header">
+    <div class="posts__sort-buttons">
+      <button class="btn posts__sort-btn active">
+        <img src="../assets/img/ico-new.svg" alt="New icon" />
+        <span>New</span>
+      </button>
+      <button class="btn posts__sort-btn">
+        <img src="../assets/img/ico-hot.svg" alt="Hot icon" />
+        <span>Hot</span>
+      </button>
+    </div>
+    <button class="btn posts__add-post-btn" @click="isPopupOpened = true">
+      Add post +
+    </button>
+    <div class="posts__popup" v-if="isPopupOpened">
+      <button class="posts__popup-close" @click="isPopupOpened = false">
+        X
+      </button>
+      <div class="posts__popup-inputs">
+        <form>
+          <label for="enter-title">Enter title:</label>
+          <input type="text" id="enter-title" v-model="title" />
+        </form>
+        <form>
+          <label for="enter-body">Enter text:</label>
+          <textarea type="text" id="enter-body" v-model="body"></textarea>
+        </form>
+        <button class="btn posts__popup-add-button" @click="addPost">
+          Add post
+        </button>
       </div>
     </div>
   </div>
+  <div class="posts__post post" v-if="!posts.length">
+    There are no posts...
+  </div>
+  <template v-else>
+    <TransitionGroup name="list">
+      <div
+        class="posts__post post"
+        v-for="(post, idx) in posts"
+        :key="post.id"
+        :data-index="idx"
+      >
+        <div class="post__info">
+          <div class="post__info_image"></div>
+          <div class="post__info_text">
+            <span class="post__id"> Id: {{ post.id }}</span>
+            <h2 class="post__title">{{ post.title }}</h2>
+            <p class="post__body">
+              {{ post.body }}
+            </p>
+            <a @click.prevent="removePost(post.id)">удалить нахуй</a>
+          </div>
+        </div>
+      </div>
+    </TransitionGroup>
+  </template>
 
   <PaginationButtons @increase-page="increasePage" @reduce-page="reducePage" />
 </template>
+
+<style scoped>
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.35s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
+}
+</style>
---
+++ src/components/SortBox.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import { defineProps } from 'vue';
+
+const props = defineProps({
+    'isAscending': Boolean,
+    'attr': String
+})
+
+const selectOptions = (e) => {
+    attr.value = e.target.value
+    isAscending.value = !isAscending.value
+};
+</script>
+
+<template>
+    <div @click="selectOptions">
+        <button value="id">Id</button>
+        <button value="title">Title</button>
+        <button value="body">Body</button>
+    </div>
+</template>
