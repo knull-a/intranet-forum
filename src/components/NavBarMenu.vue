532f57438d4cceb23e046d0e140d69ccab3aec07
k-nulla <kenjopa6@gmail.com>
2023-03-13 17:51:54 +0500

    decomposing

--- src/components/NavBarMenu.vue
+++ src/components/NavBarMenu.vue
@@ -1,8 +1,32 @@
 <script setup>
+import { ref } from 'vue';
+const emit = defineEmits(["onFileChange"])
+const props = defineProps({
+    "imageUrl": String
+})
+const onFileChange = (e) => {
+  const file = e.target.files[0]
+  props.imageUrl = URL.createObjectURL(file)
+}
+const isProfileOpened = ref(false)
 </script>
 
 <template>
-    <div class="header__logo">
+    <div class="header__logo">  
+        {{ props.imageUrl }}
         <img src="../assets/img/logo.svg" alt="Logo">
     </div>
+    <input type="file" id="profile-pic" @change="onFileChange" hidden>
+    <div class="header__profile" @mouseover="isProfileOpened = true" @mouseleave="isProfileOpened = false">
+        <label for="profile-pic" class="header__profile-photo">
+            <img :src="props.imageUrl" alt="Profile">
+        </label>
+        <div class="header__profile-dropdown" v-if="isProfileOpened">
+            <input type="text" value="Username" class="header__profile-username">
+            <p>Total Likes: </p>
+            
+        </div>
+    </div>
+    
 </template>
+
--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, watch, computed } from "vue";
+import NavbarMenu from "./NavbarMenu.vue";
 
 const selectOptions = (e) => {
   attr.value = e.target.value;
@@ -37,6 +38,7 @@ function addPost() {
     date: new Date().toLocaleString(),
     id: Number(new Date()),
     likes: likes.value,
+    username: username.value
   });
   title.value = "";
   body.value = "";
@@ -51,6 +53,9 @@ const title = ref("");
 const body = ref("");
 const isActive = ref(false);
 const likes = ref(0);
+const username = ref("hehe")
+
+const postImage = "/src/assets/img/horse.jpg"
 
 const sortPosts = () => {
   isActive.value = !isActive.value;
@@ -58,66 +63,80 @@ const sortPosts = () => {
   console.log(isHotSorted.value);
 };
 
+
+const imageUrl = ref("/src/assets/img/profile-pic.jpg")
+
+
+
 </script>
 
 <template>
-  <div class="posts__header">
-    <div class="posts__sort-buttons" @click="sortPosts">
-      <button class="btn posts__sort-btn" :class="{ active: !isActive }">
-        <img src="../assets/img/ico-new.svg" alt="New icon" />
-        <span>New</span>
-      </button>
-      <button class="btn posts__sort-btn" :class="{ active: isActive }">
-        <img src="../assets/img/ico-hot.svg" alt="Hot icon" />
-        <span>Hot</span>
-      </button>
-    </div>
-    <button class="btn posts__add-post-btn" @click="isPopupOpened = true">
-      Add post +
-    </button>
-    <div class="posts__popup" v-if="isPopupOpened">
-      <button class="posts__popup-close" @click="isPopupOpened = false">
-        X
-      </button>
-      <div class="posts__popup-inputs">
-        <form>
-          <label for="enter-title">Enter title:</label>
-          <input type="text" id="enter-title" v-model="title" />
-        </form>
-        <form>
-          <label for="enter-body">Enter text:</label>
-          <textarea type="text" id="enter-body" v-model="body"></textarea>
-        </form>
-        <button class="btn posts__popup-add-button" @click="addPost">
-          Add post
+  <header class="header">
+    <NavbarMenu @on-file-change="onFileChange" :image-url="imageUrl"/>
+  </header>
+  <main>
+    <div class="posts">
+      <div class="posts__header">
+        <div class="posts__sort-buttons" @click="sortPosts">
+          <button class="btn posts__sort-btn" :class="{ active: !isActive }">
+            <img src="../assets/img/ico-new.svg" alt="New icon" />
+            <span>New</span>
+          </button>
+          <button class="btn posts__sort-btn" :class="{ active: isActive }">
+            <img src="../assets/img/ico-hot.svg" alt="Hot icon" />
+            <span>Hot</span>
+          </button>
+        </div>
+        <button class="btn posts__add-post-btn" @click="isPopupOpened = true">
+          Add post +
         </button>
-      </div>
-    </div>
-  </div>
-  <div class="posts__post post" v-if="!posts.length">There are no posts...</div>
-  <template v-else>
-    <TransitionGroup name="list">
-      <div class="posts__post post" v-for="post in posts" :key="post.id">
-        <div class="post__info">
-          <div class="post__info_image"></div>
-          <div class="post__info_text">
-            <p>{{ post.id }}</p>
-            <span class="post__id"> {{ post.date }}</span>
-            <h2 class="post__title">{{ post.title }}</h2>
-            <p class="post__body">
-              {{ post.body }}
-            </p>
-            <a @click.prevent="removePost(post.id)">удалить нахуй</a>
+        <div class="posts__popup" v-if="isPopupOpened">
+          <button class="posts__popup-close" @click="isPopupOpened = false">
+            X
+          </button>
+          <div class="posts__popup-inputs">
+            <form>
+              <label for="enter-title">Enter title:</label>
+              <input type="text" id="enter-title" v-model="title" />
+            </form>
+            <form>
+              <label for="enter-body">Enter text:</label>
+              <textarea type="text" id="enter-body" v-model="body"></textarea>
+            </form>
+            <button class="btn posts__popup-add-button" @click="addPost">
+              Add post
+            </button>
           </div>
         </div>
-        <div class="post__likes">
-          <button class="btn post__likes-btn" @click="post.likes++">▲</button>
-          <p>{{ post.likes }}</p>
-          <button class="btn post__likes-btn" @click="post.likes--">▼</button>
-        </div>
       </div>
-    </TransitionGroup>
-  </template>
+      <div class="posts__post post" v-if="!posts.length">There are no posts...</div>
+      <template v-else>
+        <TransitionGroup name="list">
+          <div class="posts__post post" v-for="post in posts" :key="post.id">
+            <div class="post__info">
+              <div class="post__info_image">
+                <img :src="imageUrl" alt="">
+              </div>
+              <div class="post__info_text">
+                <p>{{ post.username }}</p>
+                <span class="post__id"> {{ post.date }}</span>
+                <h2 class="post__title">{{ post.title }}</h2>
+                <p class="post__body">
+                  {{ post.body }}
+                </p>
+                <a @click.prevent="removePost(post.id)">delete nafig</a>
+              </div>
+            </div>
+            <div class="post__likes">
+              <button class="btn post__likes-btn" @click="post.likes++">▲</button>
+              <p>{{ post.likes }}</p>
+              <button class="btn post__likes-btn" @click="post.likes--">▼</button>
+            </div>
+          </div>
+        </TransitionGroup>
+      </template>
+    </div>
+  </main>
 
   <PaginationButtons @increase-page="increasePage" @reduce-page="reducePage" />
 </template>
