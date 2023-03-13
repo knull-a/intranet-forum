bfb44aeace4d40bb48372b64d807937c570b4de8
k-nulla <kenjopa6@gmail.com>
2023-03-13 17:52:10 +0500

    decomposing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup>
   import PostsList from './components/PostsList.vue'
-  import NavBarMenu from './components/NavBarMenu.vue';
   
 </script>
 
 <template>
-    <header class="header">
-      <NavBarMenu />
-    </header>
-  
     <div class="wrapper">
-      <main>
-        <div class="posts">
-          <Suspense>
-            <PostsList />
-            <template #fallback>
-              Loading...
-            </template>
-          </Suspense>
-        </div>
-        
-      </main>
+      <PostsList />
     </div>
 </template>
 
