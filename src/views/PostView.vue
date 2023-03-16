d87c18eeee2ef859fd45f02926df327bbc78f13e
k-nulla <kenjopa6@gmail.com>
2023-03-16 16:22:59 +0500

    add localStorage.js

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,37 +1,61 @@
 <script setup>
-    import { useRoute } from 'vue-router';
-    import NavbarMenu from '../components/NavbarMenu.vue';
-    const route = useRoute()
-    const posts = JSON.parse(localStorage.getItem("posts")) || []
-    const imageUrl = JSON.parse(localStorage.getItem("imageUrl"))
-    console.log(imageUrl);
-    const post = posts.find(el => el.id == route.params.id)
+import { useRoute } from "vue-router";
+import NavbarMenu from "../components/NavbarMenu.vue";
+const route = useRoute();
+const posts = JSON.parse(localStorage.getItem("posts")) || [];
+const imageUrl =
+  JSON.parse(localStorage.getItem("imageUrl")) ||
+  "/src/assets/img/profile-pic.jpg";
+const username = JSON.parse(localStorage.getItem("username")) || "Username";
+const post = posts.find((el) => el.id == route.params.id);
+
+const removePost = () =>
+  localStorage.setItem(
+    "posts",
+    JSON.stringify(
+      posts.filter(
+        (item) => item !== posts.find((el) => el.id == route.params.id)
+      )
+    )
+  );
 </script>
 
 <template>
-    <header class="header">
-        <NavbarMenu :image-url="imageUrl" />
-    </header>
-    <div class="post">
-        <div class="post__info">
-            <div class="post__info_image">
-              <!-- <img :src="imageUrl" alt=""> -->
-            </div>
-            <div class="post__info_text">
-              <p>{{ post.username }}</p>
-              <span class="post__id"> {{ post.date }}</span>
-              <h2 class="post__title">{{ post.title }}</h2>
-              <p class="post__body">
-                {{ post.body }}
-              </p>
-              <a @click.prevent="removePost(post.id)">delete nafig</a>
-            </div>
-        </div>
-        <div class="post__likes">
-          <button class="btn post__likes-btn" @click="post.likes++">▲</button>
-          <p>{{ post.likes }}</p>
-          <button class="btn post__likes-btn" @click="post.likes--">▼</button>
-        </div>
-        <!-- <RouterLink to="/">Go back</RouterLink> -->
+  <header class="header">
+    <NavbarMenu :image-url="imageUrl" v-model="username" />
+  </header>
+  <div class="post">
+    <div class="post__info">
+      <div class="post__info_image">
+        <img :src="imageUrl" alt="Profile Picture" />
+      </div>
+      <div class="post__info_text">
+        <p class="post__username">{{ username }}</p>
+        <span class="post__id"> {{ post.date }}</span>
+        <h2 class="post__title">{{ post.title }}</h2>
+        <p class="post__body">
+          {{ post.body }}
+        </p>
+        
+      </div>
     </div>
+    <div class="post__likes">
+      <p>{{ post.likes }}</p>
+      <RouterLink to="/" @click.prevent="removePost" class="btn post__info_btn-delete" title="Delete post"></RouterLink>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.post {
+  &__title {
+    margin-bottom: 5px;
+  }
+  &__info_btn-delete {
+    margin-top: 5px;
+    background-image: url('../assets/img/ico-delete.png');
+    background-repeat: no-repeat;
+    background-position: 8px;
+  }
+}
+</style>
