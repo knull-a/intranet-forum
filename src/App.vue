158e30a9def320f247b79c39f35f9e0dca75a3ea
kenjopa <kenjopa6@gmail.com>
2023-03-24 05:43:19 +0500

    fix navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
   import { RouterView } from 'vue-router';
-  import NavBarMenu from './components/NavbarMenu.vue';
 </script>
 
 <template>
     <div class="wrapper">
-      <header class="header"><NavBarMenu /></header>
       <RouterView />
     </div>
 </template>
